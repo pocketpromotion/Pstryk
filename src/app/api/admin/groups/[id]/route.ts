04f7687a309c303e192d158845925af6c57d6738
Christian Meissner <cm@pocketpromotion.de>
2025-12-06 18:21:46 +0100

    deployment fix

--- src/app/api/admin/groups/[id]/route.ts
+++ src/app/api/admin/groups/[id]/route.ts
@@ -3,6 +3,9 @@ import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
+
+export const dynamic = 'force-dynamic';
+
 export async function PUT(
     req: Request,
     { params }: { params: { id: string } }
